07101e46cc81d1a69b742e4cb8ebbfc5102c3b19
andrewschwabe <schwabea530@gmail.com>
2022-05-21 15:04:18 -0500

    Add testing around not setting isLit paramatera && function parameter

--- src/Cell.test.js
+++ src/Cell.test.js
@@ -25,11 +25,38 @@ it("isLit class applied", () => {
     expect(cell.className).toContain('Cell-lit');
 });
 
-it("isLit class not applied", () => {
+it("isLit class not applied - parameter set", () => {
     act(() => {
         render(<Cell isLit={false} />, container);
     });
 
     const cell = container.querySelector('td');
     expect(cell.className).not.toContain('Cell-lit');
+});
+
+it("isLit class not applied - parameter not set", () => {
+    act(() => {
+        render(<Cell />, container);
+    });
+
+    const cell = container.querySelector('td');
+    expect(cell.className).not.toContain('Cell-lit');
+});
+
+it("click binding set", () => {
+    act(() => {
+        render(<Cell flipCellsAroundMe={() => {console.log('clicked')}}/>, container);
+    });
+
+    const cell = container.querySelector('td');
+    expect(cell.onclick).toBeDefined();
+});
+
+it("click binding not set", () => {
+    act(() => {
+        render(<Cell />, container);
+    });
+
+    const cell = container.querySelector('td');
+    expect(cell.onclick).toBeNull();
 });
