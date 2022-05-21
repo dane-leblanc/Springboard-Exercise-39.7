5dee72cfa3aac59665b528ac8c86242e4659a697
andrewschwabe <schwabea530@gmail.com>
2022-05-21 13:50:36 -0500

    Add 2 tests for Cell component

---
+++ src/Cell.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {render, unmountComponentAtNode} from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import Cell from './Cell';
+
+let container = null;
+
+beforeEach(() => {
+    container = document.createElement("tr");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+it("isLit class applied", () => {
+    act(() => {
+        render(<Cell isLit={true} />, container);
+    });
+
+    const cell = container.querySelector('td');
+    expect(cell.className).toContain('Cell-lit');
+});
+
+it("isLit class not applied", () => {
+    act(() => {
+        render(<Cell isLit={false} />, container);
+    });
+
+    const cell = container.querySelector('td');
+    expect(cell.className).not.toContain('Cell-lit');
+});
